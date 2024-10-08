044e70e4a2a87708dfca998abcd4d219f4993c47
mohamed yasser <medoyasser511@gmail.com>
2024-10-08 20:39:32 +0300

    add featured list view list

---
+++ lib/features/home/presentation/view/widgets/featured_box_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/features/home/presentation/view/widgets/featured_list_view_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class FeaturedBoxListView extends StatelessWidget {
+  const FeaturedBoxListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      //to be resposive
+      //the height of the item to the screen
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: FeaturedListViewItem(),
+            );
+          }),
+    );
+  }
+}
