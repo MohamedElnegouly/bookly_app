3dd15e44a7b0d9230cfce8b8a6e48a2ea0710940
mohamed yasser <medoyasser511@gmail.com>
2024-10-09 21:45:16 +0300

    best seller item done

--- lib/features/home/presentation/view/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/view/widgets/best_seller_list_view_item.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/rating_seller.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -34,18 +36,45 @@ class BestSellerListViewItem extends StatelessWidget {
             const SizedBox(
               width: 30,
             ),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .5,
-                  child: const Text(
-                    'Harry Potterand the Goblet of Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potterand the Goblet of Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: kGtescrafine,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Text(
+                    'J.K holing',
+                    style: Styles.textStyle14,
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 € ',
+                        style: Styles.textStyle20.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      const RatingSeller(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
