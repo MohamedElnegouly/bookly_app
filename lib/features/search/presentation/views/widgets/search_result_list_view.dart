61ab5445de748e9569d44d818a41894b056fa337
mohamed yasser <medoyasser511@gmail.com>
2024-10-14 03:09:23 +0300

    search view done

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,16 +1,15 @@
-import 'package:bookly_app/features/home/presentation/view/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+import '../../../../home/presentation/view/widgets/best_seller_list_view_item.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         // take all the height of the children and view them one time
         //shrinkWrap: true,
-        // to be not crollable because it already scroll by custom scroll view
-        physics: const NeverScrollableScrollPhysics(),
         padding: EdgeInsets.zero,
         itemCount: 10,
         itemBuilder: (context, index) {
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,18 +1,36 @@
 import 'package:bookly_app/features/search/presentation/views/widgets/custom_search_text_feild.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          CustomSearchTextFeild(),
-        ],
-      ),
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: CustomSearchTextFeild(),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            'Search result',
+            style: Styles.textStyle18,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Expanded(child: SearchResultListView()),
+      ],
     );
   }
 }
